b1d86d6d5b066cd5f095684ed721288f2eac91bd
CristianLenovoU22 <cristianoso19@gmail.com>
2023-02-21 17:37:03 -0500

    Objetos en Python

--- python/main.py
+++ python/main.py
@@ -1,2 +1,12 @@
+from car import Car
 if __name__ == "__main__":
     print("Hola mundo")
+    car = Car()
+    car.license = "PBO101"
+    car.driver = "Cristian Sacta"
+    print(vars(car))
+
+    car2 = Car()
+    car2.license = "PBO102"
+    car2.driver = "Jenny Portilla"
+    print(vars(car2))
