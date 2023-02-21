e9fbf9fd091f36fcfb7c23a1e841f6fd097cf8e0
CristianLenovoU22 <cristianoso19@gmail.com>
2023-02-21 17:50:50 -0500

    declare constructor Method

--- java/Main.java
+++ java/Main.java
@@ -1,14 +1,10 @@
 class Main { //need class name same as file name
 	public static void main (String[] args) {
 		System.out.println("Hello World");
-		Car car = new Car();
-		car.license = "PBO1001";
-		car.driver = "Cristian Sacta";
+		Car car = new Car("PBO1001", new Account("Cristian Sacta", "AND123"));
 		car.passenger = 4;
 		car.printDataCar();
-		Car car2 = new Car();
-		car2.license = "PDK1002";
-		car2.driver = "Jenny Portilla";
+		Car car2 = new Car("PBO1002", new Account("Jenny Portilla", "PDK1003"));
 		car2.passenger = 3;
 		car2.printDataCar();
 	}
