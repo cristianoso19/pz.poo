1f1d381bb75e8395177b53bf78d62e317011602d
CristianLenovoU22 <cristianoso19@gmail.com>
2023-02-21 21:30:00 -0500

    php method

--- PHP/car.php
+++ PHP/car.php
@@ -1,7 +1,20 @@
 <?php
 class Car{
     public $id;
-    public $licence;
+    public $license;
     public $driver;
     public $passanger;
+
+    public function __construct($license, $driver){
+        $this->license = $license;
+        $this->driver = $driver;
+    }
+    
+    public function printDataCar(){
+        echo "license: " . $this->license . ", conductor: " . $this->driver->name . ", document: " . $this->driver->document;
+    }
+    /* For run this code execute:
+    php -S localhost:8000 -t .
+    and go to localhost in a web browser.
+ */
 }
