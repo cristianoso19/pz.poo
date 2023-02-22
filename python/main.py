efbdfb94fafab6a8950c7fc061c51afcd9f6196e
CristianLenovoU22 <cristianoso19@gmail.com>
2023-02-21 20:49:23 -0500

    Metodo constructor Python

--- python/main.py
+++ python/main.py
@@ -1,12 +1,12 @@
 from car import Car
+from account import Account
 if __name__ == "__main__":
     print("Hola mundo")
-    car = Car()
-    car.license = "PBO101"
-    car.driver = "Cristian Sacta"
-    print(vars(car))
 
-    car2 = Car()
-    car2.license = "PBO102"
-    car2.driver = "Jenny Portilla"
+    car = Car("PBO101",Account("Cristian Sacta", "APA1919"))
+    print(vars(car))
+    print(vars(car.driver))
+
+    car2 = Car("PID232",Account("Jenny Portilla", "NDO202"))
     print(vars(car2))
+    print(vars(car2.driver))
