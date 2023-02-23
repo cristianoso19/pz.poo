773a56a1922c80470048e65a088f700bf3cfd113
CristianLenovoU22 <cristianoso19@gmail.com>
2023-02-22 21:36:30 -0500

    Herencia php

--- PHP/index.php
+++ PHP/index.php
@@ -1,7 +1,12 @@
 <?php
 require_once('car.php');
+require_once('uberX.php');
+require_once('uberPool.php');
 require_once('account.php');
 
-$car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
+$uberX = new UberX("CVB123", new Account("Andres Herrera", "AND123"),"Acura","RTX");
+//$car = new Car("AW456", new Account("Andres Herrera", "AMS123"));
+$uberX->printDataCar();
 
-$car->printDataCar();
+$uberPool = new UberPool("PBO1001", new Account("Jenny Portilla", "AND123"),"Audi","R5");
+$uberPool->printDataCar();
