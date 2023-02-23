664c74fa8ea043a946f14b57c839b70ee3e3ae92
CristianLenovoU22 <cristianoso19@gmail.com>
2023-02-22 22:29:18 -0500

    Herencia en Js

--- JS EC6/index.js
+++ JS EC6/index.js
@@ -1,6 +1,11 @@
 const Car = require('./Car');
-const Account = require('./Account');
+const Account = require ('./Account');
+const UberX = require ('./UberX');
 
 var car = new Car("PBO1010", new Account ("Cristian Sacta", "PGO1002"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX("AWD439", new Account("Andrea Ferran", "ADP232"), "Chevrolet", "Spark")
+uberX.passenger = 4;
+uberX.printDataCar();
