17103d9fed00ad091fcaf1dbf69d978cb1ca1949
CristianLenovoU22 <cristianoso19@gmail.com>
2023-02-22 21:53:45 -0500

    Herencia en python

--- python/main.py
+++ python/main.py
@@ -1,5 +1,6 @@
 from car import Car
 from account import Account
+from UberBlack import UberBlack
 if __name__ == "__main__":
     print("Hola mundo")
 
@@ -10,3 +11,6 @@ if __name__ == "__main__":
     car2 = Car("PID232",Account("Jenny Portilla", "NDO202"))
     print(vars(car2))
     print(vars(car2.driver))
+
+    carBlack = UberBlack("PSI1010","Cristian","RTX","Soft") 
+    print(vars(carBlack))
